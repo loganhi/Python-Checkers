5bba47f46f8467536f793616c3e3a7aaf381428e
Logan <Loganstoddard5@gmail.com>
2022-03-10 21:37:20 -0500

    first commit

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+RED = (194, 56, 56)
+WHITE = (255, 255, 255)
+TAN = (220, 170, 160)
+BLACK = (30, 30, 30)
+BLUE = (0, 0, 255)
+GREY = (82, 99, 110)
+
+CROWN = pygame.transform.scale(pygame.image.load('C:\\Users\\Logan\\Documents\\Python Checkers\\checkers\\assets\\crown.png'), (45, 25))
